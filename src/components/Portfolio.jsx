3edb1a670ca6dea5f4be9a1460b539cea98df6d2
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-11-30 17:43:44 -0500

    debugging initial file connections, rendered some stuff

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
-function Portfolio(props) {
-    
+function Portfolio() {
+    const heading = "Elizabeth Tucker's Portfolio";
+
+    return (
+        <div>
+            <h2>{heading}</h2>
+        </div>
+    );
 }
+
+export default Portfolio;
