e7046b49d79bf5861b0965c3cc77a3f4b40652b7
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-11-30 17:16:35 -0500

    initial file setup and boilerplater coding

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDom.createRoot(document.getElementBy('root')).render(
+    <App />
+)
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { definteConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins : [react()],
+    server: {
+        port: 3000,
+        open: true
+    }
+})
